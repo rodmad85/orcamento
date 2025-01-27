10835db5a9b51c1cfb3b214923dc3b37e1e830c3
rodrigo <rodrigomadu85@gmail.com>
2025-01-27 08:02:26 -0300

    [FIX] Corrigido visualização (readonly)

--- models/orcamento.py
+++ models/orcamento.py
@@ -1,5 +1,3 @@
-import time
-from datetime import datetime
 from odoo import fields, models, api
 from odoo.exceptions import ValidationError
 
@@ -135,6 +133,7 @@ class OrcaTabela(models.TransientModel):
     resulv = fields.Monetary(string="Resultado Venda")
     resuli = fields.Monetary(string="Resultado Industrialização")
     resuls = fields.Monetary(string="Resultado Serviço")
+    readonly = fields.Boolean(string="readonly")
 
     # horas_prev = fields.One2many('orca.horas.prev', 'funcionario',string='Horas Previstas')
 
@@ -144,58 +143,31 @@ class OrcaTabela(models.TransientModel):
 
     def default_get(self, fields):
 
-        pedido = self.env.context.get('active_id')
-        mes = datetime.now().month - 2
-        ano = datetime.now().year
-        dt_str = str(ano)+ '-' + str(mes).zfill(2) + '-' + '01'
-        city_comp = self.env.company.city_id
-        order = self.env['sale.order.line'].browse(pedido).order_id
-        partner = order.partner_id
-        if partner.is_company == True:
-            if partner.city_id:
-                city_part = partner.city_id
-            else:
-                raise ValidationError("Defina a cidade do contato.")
-
-        elif partner.parent_id:
-            if partner.parent_id.city_id:
-                city_part = partner.parent_id.city_id
-            else:
-                raise ValidationError("Defina a cidade do contato.")
-
-
-
-
-        if city_part == city_comp:
-            ret = True
-        else:
-            ret = False
-
         res = super(OrcaTabela, self).default_get(fields)
         value = self.env['ir.config_parameter'].sudo().get_param('orcamento.custo_fixo')
-
-        vvenda = self.env['os.impostos.line'].search([('fiscal_position.name','=','Venda'),('mes','=',dt_str)]).percentual
+        vvenda = self.env['ir.config_parameter'].sudo().get_param('orcamento.venda')
         vhora = self.env['ir.config_parameter'].sudo().get_param('orcamento.vhora')
-
-        if ret:
-            vservico = self.env['os.impostos.line'].search([('fiscal_position.name', '=', 'Serviço c retenção'), ('mes', '=', dt_str)]).percentual
-        else:
-            vservico = self.env['os.impostos.line'].search([('fiscal_position.name', '=', 'Serviço'), ('mes', '=', dt_str)]).percentual
-
-        vindus = self.env['os.impostos.line'].search([('fiscal_position.name','=','Industrialização'),('mes','=',dt_str)]).percentual
-
-
+        vservico = self.env['ir.config_parameter'].sudo().get_param('orcamento.servico')
+        vindus = self.env['ir.config_parameter'].sudo().get_param('orcamento.industrializacao')
+        pedido = self.env.context.get('active_id')
         horas = self.env['sale.order.line'].browse(pedido).mo
         horast = self.env['sale.order.line'].browse(pedido).mo_total
         matp = self.env['sale.order.line'].browse(pedido).mp
         lucro = self.env['sale.order.line'].browse(pedido).lucro
         terc = self.env['sale.order.line'].browse(pedido).terc
         movalor = self.env['sale.order.line'].browse(pedido).mo_valor
+        order = self.env['sale.order.line'].browse(pedido).order_id.id
+        order_status = self.env['sale.order'].browse(order).state
+
+        if order_status == "sale":
+            self.readonly = True
+        else:
+            self.readonly = False
+
 
         if horas or matp or terc:
             res.update({
                 'mp': matp,
-                'mo': horas,
                 'terc': terc,
                 'lucro': lucro,
                 'mo_total': horast,
@@ -205,6 +177,7 @@ class OrcaTabela(models.TransientModel):
                 'imposto_serv': vservico,
                 'imposto_ind': vindus,
                 'linha': pedido,
+                'readonly': self.readonly
 
             })
         else:
@@ -216,6 +189,7 @@ class OrcaTabela(models.TransientModel):
                     'imposto_serv': vservico,
                     'imposto_ind': vindus,
                     'linha': pedido,
+                    'readonly': self.readonly,
 
                 })
 
@@ -321,18 +295,9 @@ class OrcaTabela(models.TransientModel):
         ci = (self.valor_ind * (self.custos / 100))
         cs = (self.valor_serv * (self.custos / 100))
 
-        if self.valor_venda and self.imposto_venda:
-            impv = self.valor_venda / self.imposto_venda
-        else:
-            impv = 0
-        if self.valor_serv and self.imposto_serv:
-            imps = self.valor_serv / self.imposto_serv
-        else:
-            imps = 0
-        if self.valor_ind and self.imposto_ind:
-            impi = self.valor_ind / self.imposto_ind
-        else:
-            impi = 0
+        impv = self.valor_venda / self.imposto_venda
+        imps = self.valor_serv / self.imposto_serv
+        impi = self.valor_ind / self.imposto_ind
 
         self.write({'cvenda': cv, 'cind': ci, 'cserv': cs})
         self.write({'impv_valor': impv, 'impi_valor': impi, 'imps_valor': imps})
